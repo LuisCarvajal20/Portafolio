8ceb85f2c34c7c13945dfd73bc318831de135f28
Luis Carvajal <luisalejandrocarvajal16@gmail.com>
2025-11-20 19:21:36 -0500

    se arreglaron muchas cosas como el background

--- Main.js
+++ Main.js
@@ -1,3 +1,15 @@
-document.getElementById("menu-toggle").onclick = () => {
-    document.getElementById("menu").classList.toggle("show");
+const btn = document.getElementById("menu-toggle");
+const menu = document.getElementById("navbar");
+const overlay = document.getElementById("overlay");
+
+btn.onclick = () => {
+  btn.classList.toggle("open");
+  menu.classList.toggle("open");
+  overlay.classList.toggle("active");
+};
+
+overlay.onclick = () => {
+  btn.classList.remove("open");
+  menu.classList.remove("open");
+  overlay.classList.remove("active");
 };
